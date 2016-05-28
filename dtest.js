7c3e7ff5b5543aae7efce0d9b33b589adb63eecf
avnshpnwar <avnshpnwar@users.noreply.github.com>
2016-05-28 09:26:51 +0530

    Added one more step

--- dtest.js
+++ dtest.js
@@ -3,27 +3,62 @@ var cpr = "";
 var otp = "";
 
 //casper.options.colorizerType = "Dummy";
+//casper.options.logLevel =  "debug";
 
-casper.test.begin('NO BankId Logon', 3, function suite(test) {
+casper.test.begin('NO BankId Logon', function suite(test) {
 
     casper.start(url, function() {
         test.assertHttpStatus(200, "Page Loaded Successfully");
-				test.assertTitle("Freaky Friday!", "Title is OK");
     });
 
     casper.wait(2000, function() {
         this.page.switchToFrame(1);
     });
 
-    casper.waitFor(function check() {
-        if (this.exists('.wrp')) return true;
-        else return false;
-    }, function then() {
-				test.assertExists('.wrp', "Iframe Switched Successfully")
-		}, function timeout() {
-				test.done();
-		}, 10000);
-
+	casper.waitFor(function check() {
+		if (this.exists('.wrp')) {
+			var lab = this.getElementInfo('form label');
+			if (lab.text === "Fødselsnummer") {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}, function then() {
+		test.assertExists('.wrp', "Iframe Switched Successfully")
+	}, function timeout() {
+		test.done();
+	}, 10000);
+	
+	casper.then(function () {
+	    var inputId = "#" + this.getElementAttribute('.wrp input', 'id');
+	    this.sendKeys(inputId, cpr, {keepFocus: true});
+	    this.sendKeys('button[title="Neste"]', casper.page.event.key.Enter, {keepFocus: true});
+	});
+	
+	casper.waitFor(function check() {
+		if (this.exists('.wrp')) {
+			var lab = this.getElementInfo('form label');
+			if (lab.text === "Engangskode") {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}, function then() {
+		test.assertSelectorHasText('form label', 'Engangskode', "CPR entered successfully");
+	}, function timeout() {
+		test.done();
+	}, 10000);
+	
+	casper.wait(2000, function() {
+		img = gallery + "button.png";
+		this.capture(img);
+	});
     casper.run(function() {
         test.done();
     });
